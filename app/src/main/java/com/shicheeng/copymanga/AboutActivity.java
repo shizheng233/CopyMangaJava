46aa2f6fa051d76cd8411ca99f160caffe7ed030
shizheng233 <2645512596@qq.com>
2022-03-31 12:05:01 +0800

    更新介绍页面

--- app/src/main/java/com/shicheeng/copymanga/AboutActivity.java
+++ app/src/main/java/com/shicheeng/copymanga/AboutActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 import com.drakeet.about.AbsAboutActivity;
+import com.drakeet.about.Card;
 import com.drakeet.about.Category;
 import com.drakeet.about.License;
 
@@ -17,13 +18,17 @@ public class AboutActivity extends AbsAboutActivity {
     @Override
     protected void onCreateHeader(@NonNull ImageView icon, @NonNull TextView slogan, @NonNull TextView version) {
         icon.setImageResource(R.mipmap.ic_launcher_copy);
-        slogan.setText(R.string.app_name);
+        slogan.setText(R.string.li_des);
         version.setText("v "+BuildConfig.VERSION_NAME);
 
     }
 
     @Override
     protected void onItemsCreated(@NonNull List<Object> items) {
+        items.add(new Category(getString(R.string.description_text)));
+        items.add(new Card(getString(R.string.des)));
+
+
         items.add(new Category(getString(R.string.open_source)));
         items.add(new License("MultiType", "drakeet", License.APACHE_2, "https://github.com/drakeet/MultiType"));
         items.add(new License("about-page", "drakeet", License.APACHE_2, "https://github.com/drakeet/about-page"));
